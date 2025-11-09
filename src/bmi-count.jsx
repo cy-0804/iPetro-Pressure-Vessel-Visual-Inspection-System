7da1396f140037b04b7d3a245be6c5974c14088b
hanzxr-git <mohdfarhan7440@gmail.com>
2025-11-09 20:24:55 +0800

    siap edit title

--- src/bmi-count.jsx
+++ src/bmi-count.jsx
@@ -33,7 +33,7 @@ const BmiCalculator = () => {
 
   return (
     <div className='container'>
-      <h1>BMI Calculator</h1>
+      <h1>BMI Calculator TESTTT</h1>
       <div className='input-group'>
         <label>
           Weight (kg):
